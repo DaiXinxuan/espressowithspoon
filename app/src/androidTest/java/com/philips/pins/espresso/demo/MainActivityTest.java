c5a8a96693e96a061471761ba840b8724104f9ca
daixinxuan <xinxuan.dai@philips.com>
2016-02-29 10:22:52 +0800

    update Test

--- app/src/androidTest/java/com/philips/pins/espresso/demo/MainActivityTest.java
+++ app/src/androidTest/java/com/philips/pins/espresso/demo/MainActivityTest.java
@@ -4,16 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -22,32 +19,23 @@ import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by 310231492 on 2016/2/26.
  */
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class MainActivityTest {
     private Activity activity;
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class,true,false);
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, true, false);
 
     @Before
     public void setUp() throws Exception {
-//        Intent intent = new Intent();
-//        mActivityRule .launchActivity(intent);
-//        activity = mActivityRule.getActivity();
-//        onView(withId(R.id.username)).perform(clearText());
-//        onView(withId(R.id.password)).perform(clearText());
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-
+        Intent intent = new Intent();
+        mActivityRule .launchActivity(intent);
+        activity = mActivityRule.getActivity();
     }
 
     /**
@@ -55,121 +43,116 @@ public class MainActivityTest {
      */
     @Test
     public void test1() {
-        Intent intent = new Intent();
-        mActivityRule .launchActivity(intent);
-        activity = mActivityRule.getActivity();
         onView(withId(R.id.login)).perform(click());
         try {
-            Thread.sleep(3000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        onView(withText (activity.getString(R.string.empty_user) ))
-                .inRoot(withDecorView( not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
-                .check(matches(isDisplayed ()));
-
+        onView(withText (activity.getString(R.string.empty_user)))
+                .inRoot(withDecorView(not(is(activity.getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
     }
 
-//    /**
-//     * test password empty
-//     */
-//    @Test
-//    public void test2() {
-//        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
-//        onView(withId(R.id.login)).perform(click());
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        onView(withText(activity.getString(R.string.empty_pass))).
-//                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
-//    }
-//
-//    /**
-//     * test user regist
-//     */
-//    @Test
-//    public void test3() {
-//        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
-//        onView(withId(R.id.password)).perform(typeText("19950213"),closeSoftKeyboard());
-//        onView(withId(R.id.regist)).perform(click());
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        onView(withText(activity.getString(R.string.regist_success))).
-//                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
-//    }
-//
-//    /**
-//     * test existing user regist
-//     */
-//    @Test
-//    public void test4() {
-//        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
-//        onView(withId(R.id.password)).perform(typeText("19950212"),closeSoftKeyboard());
-//        onView(withId(R.id.regist)).perform(click());
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        onView(withText(activity.getString(R.string.user_resued))).
-//                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
-//    }
-//
-//    /**
-//     * test existing user login
-//     */
-//    @Test
-//    public void test5() {
-//        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
-//        onView(withId(R.id.password)).perform(typeText("19950213"),closeSoftKeyboard());
-//        onView(withId(R.id.login)).perform(click());
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        onView(withText(activity.getString(R.string.login_success))).
-//                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
-//    }
-//
-//    /**
-//     * test nonexisten user login
-//     */
-//    @Test
-//    public void test6() {
-//        onView(withId(R.id.username)).perform(typeText("18781985618"),closeSoftKeyboard());
-//        onView(withId(R.id.password)).perform(typeText("19950213"),closeSoftKeyboard());
-//        onView(withId(R.id.login)).perform(click());
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        onView(withText(activity.getString(R.string.nonexisten_user))).
-//                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
-//    }
-//
-//    /**
-//     * test user login with wrong password
-//     */
-//    @Test
-//    public void test7() {
-//        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
-//        onView(withId(R.id.password)).perform(typeText("19950212"),closeSoftKeyboard());
-//        onView(withId(R.id.login)).perform(click());
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        onView(withText(activity.getString(R.string.login_fail))).
-//                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
-//    }
 
+    /**
+     * test password empty
+     */
+    @Test
+    public void test2() {
+        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
+        onView(withId(R.id.login)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText(activity.getString(R.string.empty_pass))).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
 
+    /**
+     * test user regist
+     */
+    @Test
+    public void test3() {
+        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("19950213"),closeSoftKeyboard());
+        onView(withId(R.id.regist)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText(activity.getString(R.string.regist_success))).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    /**
+     * test existing user regist
+     */
+    @Test
+    public void test4() {
+        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("19950212"),closeSoftKeyboard());
+        onView(withId(R.id.regist)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText(activity.getString(R.string.user_resued))).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    /**
+     * test existing user login
+     */
+    @Test
+    public void test5() {
+        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("19950213"),closeSoftKeyboard());
+        onView(withId(R.id.login)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText(activity.getString(R.string.login_success))).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    /**
+     * test nonexisten user login
+     */
+    @Test
+    public void test6() {
+        onView(withId(R.id.username)).perform(typeText("18781985618"),closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("19950213"),closeSoftKeyboard());
+        onView(withId(R.id.login)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText(activity.getString(R.string.nonexisten_user))).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    /**
+     * test user login with wrong password
+     */
+    @Test
+    public void test7() {
+        onView(withId(R.id.username)).perform(typeText("18781985619"),closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("19950212"),closeSoftKeyboard());
+        onView(withId(R.id.login)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText(activity.getString(R.string.login_fail))).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
 }
